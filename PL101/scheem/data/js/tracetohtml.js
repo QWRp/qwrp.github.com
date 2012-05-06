e76d32c11e6bf96fb30ded29c418ef09d0a7cda4
Adrian Lebioda <adrianlebioda@gmail.com>
2012-05-06 23:14:14 +0200

    Now interpreter generates all necessary strings for trace.

--- PL101/scheem/data/js/tracetohtml.js
+++ PL101/scheem/data/js/tracetohtml.js
@@ -5,23 +5,6 @@ var traceToHTML = (function () {
         return JSON.stringify(env).replace(/"/g, "'");
     }
 
-    function makeValueString(value) {
-        if (Array.isArray(value)) {
-            var string = "(";
-
-            for(var i = 0; i < value.length; i++) {
-                string += makeValueString(value[i]) + " ";
-            }
-
-            string = string.substr(0, string.length - 1) + ")";
-            return string;
-        } else if (typeof value == 'boolean') {
-            return (value) ? '#t' : '#f';
-        } else {
-            return "" + value;
-        }
-    }
-
     function processElement(trace, noexpr) {
         if (noexpr !== true && Array.isArray(trace.expr)) {
             if (trace.expr[0] === 'quote') {
@@ -57,36 +40,7 @@ var traceToHTML = (function () {
         }
     }
 
-    function stringifyTrace(trace) {
-        trace.value_string = makeValueString(trace.value);
-        if (Array.isArray(trace.expr)) {
-            if (trace.expr[0] === 'quote') {
-                trace.string = "'";
-                if(trace.value_string[0] === '(') {
-                    trace.string += trace.value_string;
-                } else {
-                    trace.string += "(" + trace.value_string + ")";
-                }
-            }
-            else {
-                var result = '(' + trace.expr[0];
-
-                for (var i = 0; i < trace.children.length; i++) {
-                    stringifyTrace(trace.children[i]);
-                    result += ' ' + trace.children[i].string;
-                }
-
-                trace.string = result + ')';
-            }
-        } else if (typeof trace.expr === 'boolean') {
-            trace.string = '' + (trace.value === true) ? '#t' : '#f';
-        } else {
-            trace.string = '' + trace.expr;
-        }
-    }
-
     function traceToHTML(trace) {
-        stringifyTrace(trace);
         return processElement(trace);
     }
 
